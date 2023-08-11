22b677dc35491d446800ea3b58dd16b0b5d61a16
fietjeOS <69521879+fietjeOS@users.noreply.github.com>
2023-08-11 16:39:41 +0300

    fixed time.sleep

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_check_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(15)
+    time.sleep(30)
     button_add_to_basket = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
     assert button_add_to_basket, f'Страница товара на сайте {link} не содержит кнопки добавления в корзину'
